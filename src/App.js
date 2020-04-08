e2f8c58b4ca36b3987d7d59101078cad6934e499
Lopamudra Mishra <lopamudra5346@gmail.com>
2020-04-08 23:26:49 +0530

    lecture- 185

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuider/BurgerBuilder';
 
 class App extends Component {
+
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+           <BurgerBuilder />    
         </Layout>
       </div>
     );
