6b2bcf758ceced09f2a1bbdf3717cb3b6f421809
Lopamudra Mishra <lopamudra5346@gmail.com>
2020-04-22 22:48:02 +0530

    229

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () =>(
-<ul className={classes.NavigationItems}>
-<NavigationItem link= "/" active> Burger Buider</NavigationItem>
-<NavigationItem link= "/">Checkout</NavigationItem>
-</ul>
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>Burger Buider</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
+    </ul>
 );
 
 export default navigationItems;
